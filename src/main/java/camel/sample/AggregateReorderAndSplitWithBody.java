d9513e9ac38eacdb974e97322275e552de390f2d
Lukasz Rozek <lukasz.rozek84@gmail.com>
2015-12-16 20:32:54 +0100

    splitting fix, headers are untouched, entire messages taken into account

--- src/main/java/camel/sample/AggregateReorderAndSplitWithBody.java
+++ src/main/java/camel/sample/AggregateReorderAndSplitWithBody.java
@@ -8,9 +8,9 @@ import java.util.TreeMap;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.file.GenericFile;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
 
@@ -68,7 +68,7 @@ public class AggregateReorderAndSplitWithBody {
     //########
     //######## Processing file: GenericFile[c:\temp\camel\camelIn\data_c2_07.txt]
     //########
-    
+
     public static void main( String[] args ) throws Exception {
         CamelContext camelContext = new DefaultCamelContext();
 
@@ -77,19 +77,17 @@ public class AggregateReorderAndSplitWithBody {
             public void configure() throws Exception {
                 //@formatter:off
                 //NOTE: this is not important - this simulates real fetching files, which should already be implemented for real
-            	//create such folder and copy content of messages.zip into it
+                //create such folder and copy content of messages.zip into it
                 from( "file:c:/temp/camel/camelIn?noop=true" )
                 
                 //START transformation
                 
                 //we want to make messages to be ordered in batches of BATCH_SIZE, so we can first process BATCH_SIZE messages for client1, then client2, ... clientX, then back to client1, client2,...
-                //to achieve this we will aggregate content of all messages (which are files in our case), resort them in batches, then split them again into separate files, so they can be processed as before
-                //in this way this approach aims to be transparent to currently existing processing code, with one IMPORTANT difference: headers of messages will be gone (!)
-                //so if real processing uses them it will break and it needs to be modified (I think all needed data can be fetched from GenericFile anyway)
-                //I could not make it work in such way that headers are preserved - see below for explanations
+                //to achieve this we will aggregate X messages, resort them in batches, then split them again into separate messages, so they can be processed as before
+                //in this way this approach aims to be transparent to currently existing processing code
                 
                 //first we need to set some header on each Message that will allow for aggregating them together (all Messages with same header will be aggregated into one)
-                //NOTE: this is in general not necessary as we want to always group all Messages, but version of aggregate where we do not pass correlactionExpression did not work for me for whatever reason
+                //FIXME: this is in general not necessary as we want to always group all Messages, but version of aggregate where we do not pass correlactionExpression did not work for me for whatever reason
                 //so we are left with setting some "dummy" header value to all Messages
                 .process( new Processor() {
                     @Override
@@ -98,41 +96,40 @@ public class AggregateReorderAndSplitWithBody {
                     }
                 })
                 
+                //FIXME: as aggregator is stateful we should connect it to some persistence mechanism
                 //aggregation itself - we aggregate together Messages that have same DUMMY_AGGREGATE_ID_HEADER_NAME (which in our case are all messages)
-                //using aggregation strategy CombineResultsAggregationStrategym which just simply adds up content of all messages together in a list
-                //please note that we add CONTENT of messages together, so we loose all headers...
+                //using aggregation strategy CombineResultsAggregationStrategym which just simply adds up all Messages together in a list
                 .aggregate( header(DUMMY_AGGREGATE_ID_HEADER_NAME), new CombineResultsAggregationStrategy() )
-                  //this parameter is important - it tells aggregator a number of messages after which it should run aggregation
-                  //if value is too small (e.g. '1') then batching is useless, as each message will be processed by its own
+                  //this parameter is important - it tells aggregator a number of Messages after which it should run aggregation
+                  //if value is too small (e.g. '1') then batching is useless, as each Message will be processed by its own
                   //aggregator will not start processing messages until NUM_OF_MESSAGES_AFTER_WHICH_AGGREGATOR_WILL_RUN is achieved (unless timeout is also set - see below)
                   .completionSize( NUM_OF_MESSAGES_AFTER_WHICH_AGGREGATOR_WILL_RUN )
-                  //second param that controls aggregation
+                  //second parameter that controls aggregation
                   //after this timeout aggregator will run anyway, even if NUM_OF_MESSAGES_AFTER_WHICH_AGGREGATOR_WILL_RUN is not achieved
                   //we need this parameter also to avoid situation when there is not enough message and they wait too long not being processed
                   .completionTimeout( TIMEOUT_AFTER_WHICH_AGGREGATOR_WILL_RUN )
                 
-                //here we are done with aggregation, we have one Message, where content of all previous Messages is aggregated in a list
+                //here we are done with aggregation, we have one Message, where all previous Messages are aggregated in a list
                 //now we need to sort this list, so content is ordered in batches of BATCH_SIZE per each client
                 .process( new Processor() {
                     @Override
                     public void process( Exchange exchange ) throws Exception {
-                        //as an input we have a list of contents of all messages (List of File representations)
-                        List<GenericFile<?>> filesInOrderTheyArrived = exchange.getIn().getBody( List.class );
+                        //as an input we have a list of all Messages
+                        List<Message> messagesInOrderTheyArrived = exchange.getIn().getBody( List.class );
                         //custom class to do sorting in batches does the sort
-                        FilesSorter sorter = new FilesSorter(filesInOrderTheyArrived, BATCH_SIZE);
-                        List<GenericFile<?>> sortedFiles = sorter.sort();
+                        MessageSorter sorter = new MessageSorter(messagesInOrderTheyArrived, BATCH_SIZE);
+                        List<Message> sortedMessages = sorter.sort();
                         //we exchange the content of body for a list where files are sorted in batches
-                        exchange.getIn().setBody( sortedFiles );
+                        exchange.getIn().setBody( sortedMessages );
                     }
                 })
                 
-                //here is the splitting of GenericFiles into separate Messages, so processing can resume as it was before (unless it uses headers - see below) 
+                //here is the splitting of GenericFiles into separate Messages, so processing can resume as it was before 
                 .split(body() )
                 
                 //END transformation
                 
                 //now we continue with real processing, which should already be implemented for real
-                //IMPORTANT: we loose everything except body of the message - all headers are gone! So if further processing uses headers it will break
                 .process( new Processor() {
                     @Override
                     public void process( Exchange exchange ) throws Exception {
@@ -148,56 +145,50 @@ public class AggregateReorderAndSplitWithBody {
         camelContext.stop();
     }
     
-    //EXPLANATION: why do we loose headers
     //class that will be used by by aggregator to combine multiple messages into one
-    //note that I combine into a list BODY of all messages and not all Messages
-    //when I wanted to create a List<Message> by putting into list objects taken from newExchange.getIn() call
-    //then my first element got 'screwed up' and it contained the entire list of files once again
-    //this is because my aggregation strategy implementation adds up values to the first element - combining it with others (this is how it usually works in examples I found on the net)
-    //to make this work properly with List<Message> I would have to probably create my own instance of Exchange
-    //I do not know Camel so well to try this and if headers are not used in further processing then this approach is enough ayway
-    //if Headers are used in further processing then there are 2 options:
-    //1. Rewrite processing so it uses body (GenericFile) to get values that were taken before from headers
-    //2. Rewrite aggregator so it aggregates a List<Message> not List<GenericFile>
+    //important is that we do not reuse first incoming message as an aggregation vessel (as most examples I found show)
+    //instead ww create a new element that will be used as aggregation vessel
     protected static class CombineResultsAggregationStrategy implements AggregationStrategy {
         @Override
         public Exchange aggregate( Exchange oldExchange, Exchange newExchange ) {
             //this piece of code is executed only for the first element in aggregation
-            //in such case I reuse the newExchange object to be my aggregated element by setting a list as its body and by adding its current body to the list
+            //IMPORTANT: we do a copy of the first message and use it as an aggregation start
+            //if we just reuse newExchange as-is then it will be messed up
             if (oldExchange == null ) {
-                List<Object> newList = new ArrayList<>();
-                newList.add( newExchange.getIn().getBody() );
-                newExchange.getIn().setBody( newList );
-                return newExchange;
+                oldExchange = newExchange.copy();
+                List<Message> newList = new ArrayList<>();
+                oldExchange.getIn().setBody( newList );
+                newList.add( newExchange.getIn() );
+                return oldExchange;
             }
             //in case it is n-th element where n > 1 I just add up content of the Message to existing list taken from aggregated Message
-            List<Object> existingList = oldExchange.getIn().getBody( List.class );
-            existingList.add( newExchange.getIn().getBody() );
+            List<Message> existingList = oldExchange.getIn().getBody( List.class );
+            existingList.add( newExchange.getIn());
             return oldExchange;
         }
     }
     
     //class that sorts messages in batches of X per each clientId (whatever clientId is and however it is obtained - this is implementation specific and I can't know this)
-    protected static class FilesSorter {
+    protected static class MessageSorter {
         
-        public FilesSorter(List<GenericFile<?>> messages, int batchSize) {
+        public MessageSorter(List<Message> messages, int batchSize) {
             this.messages = messages;
             this.batchSize = batchSize;
         }
         
-        public List<GenericFile<?>> sort() {
+        public List<Message> sort() {
             
             //split messages per client
-            TreeMap<String, List<GenericFile<?>>> messagesPerClientId = splitPerClient();
+            TreeMap<String, List<Message>> messagesPerClientId = splitPerClient();
 
             //reorder messages so they are in batches of batchSize per client:
             //client1_batch1, client2_batch1, ... , clientN_batch1, client1_batch2, client2_batch_2, ... , client1_batch3 ... 
-            List<GenericFile<?>> result = new ArrayList<>();
+            List<Message> result = new ArrayList<>();
             //sorting ends when messages for all clients were distributed
             while (!messagesPerClientId.isEmpty()) {
-                for (Iterator<Entry<String, List<GenericFile<?>>>> it = messagesPerClientId.entrySet().iterator();it.hasNext();) {
+                for (Iterator<Entry<String, List<Message>>> it = messagesPerClientId.entrySet().iterator();it.hasNext();) {
                     
-                    List<GenericFile<?>> messagesForClient = it.next().getValue();
+                    List<Message> messagesForClient = it.next().getValue();
                     
                     //we have exhausted all messages for given client, so our batch  for this client will have less than batchSize elements
                     //we remove client from the sorting
@@ -207,7 +198,7 @@ public class AggregateReorderAndSplitWithBody {
                     }
                     //take 'batchSize' number of elements for given client and remove them from list of messages for client that remain
                     else {
-                        List<GenericFile<?>> messagesToBatch = messagesForClient.subList( 0, batchSize );
+                        List<Message> messagesToBatch = messagesForClient.subList( 0, batchSize );
                         result.addAll( messagesToBatch );
                         //NOTE: this will effectively remove elements we took from messagesForClient list
                         messagesToBatch.clear();
@@ -220,13 +211,13 @@ public class AggregateReorderAndSplitWithBody {
 
         //simple transformation that elements elements per client
         //would have been shorter if I could use lambdas... ;-)
-        private TreeMap<String, List<GenericFile<?>>> splitPerClient() {
-            TreeMap<String, List<GenericFile<?>>> messagesPerClientId = new TreeMap<>();
-            for (GenericFile<?> message : messages) {
+        private TreeMap<String, List<Message>> splitPerClient() {
+            TreeMap<String, List<Message>> messagesPerClientId = new TreeMap<>();
+            for (Message message : messages) {
                 String clientId = getClientId(message);
                 //this is my dummy implementation to find out to which client message belongs
                 //depending on where this info really is has to be done differently
-                List<GenericFile<?>> messagesForClientId = messagesPerClientId.get( clientId );
+                List<Message> messagesForClientId = messagesPerClientId.get( clientId );
                 if ( null == messagesForClientId) {
                     messagesForClientId = new ArrayList<>();
                     messagesPerClientId.put( clientId, messagesForClientId );
@@ -241,11 +232,11 @@ public class AggregateReorderAndSplitWithBody {
         //important is to extract {clientId} - which I will use as identifier to which client file belongs
         //so I assume that info about to which client file belongs is in file name
         //if it is not so - then we need to extract this info from elsewhere (file path, file content - I can't say)
-        private String getClientId(GenericFile<?> message) {
-            return message.getFileNameOnly().substring( 5, 7 );
+        private String getClientId(Message message) {
+            return message.getHeader( "CamelFileName", String.class ).substring( 5, 7 );
         }
         
-        private List<GenericFile<?>> messages;
+        private List<Message> messages;
         private int batchSize;
     }
 
